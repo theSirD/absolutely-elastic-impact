fe3164415a3affbb8ef226a058a8cb734beaec32
theSirD <isaev-daniel17@yandex.ru>
2023-12-25 01:09:48 +0300

    feat: mass has meaning

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ import pymunk.pygame_util
 
 RES = WIDTH, HEIGHT = 900, 600
 FPS = 60
+
 SIDE_LENGTH_OF_SMALL_SQUARE = 50
 SIDE_LENGTH_OF_BIG_SQUARE = 100
-VELOCITY_OF_SMALL_SQUARE = (0, 0)
-VELOCITY_OF_BIG_SQUARE = (-500, 0)
 MASS_OF_SMALL_SQUARE = 1
-MASS_OF_BIG_SQUARE = 10000
+MASS_OF_BIG_SQUARE = 1
+VELOCITY_OF_SMALL_SQUARE = (0, 0)
+VELOCITY_OF_BIG_SQUARE = (-250, 0)
 
+THICKNESS_OF_STATIC_SEGMENTS = 2
 
-THICKNESS_OF_STATIC_SEGMENTS = 5
+FRICTION = 0
 
 def game():
-    little_square = Square(SIDE_LENGTH_OF_SMALL_SQUARE, SIDE_LENGTH_OF_SMALL_SQUARE/2 + THICKNESS_OF_STATIC_SEGMENTS, SIDE_LENGTH_OF_SMALL_SQUARE/2 + THICKNESS_OF_STATIC_SEGMENTS, 1, VELOCITY_OF_SMALL_SQUARE, MASS_OF_SMALL_SQUARE)
-    big_square = Square(SIDE_LENGTH_OF_BIG_SQUARE, SIDE_LENGTH_OF_BIG_SQUARE/2 + WIDTH - SIDE_LENGTH_OF_BIG_SQUARE, SIDE_LENGTH_OF_BIG_SQUARE/2 + THICKNESS_OF_STATIC_SEGMENTS, 1, VELOCITY_OF_BIG_SQUARE, MASS_OF_BIG_SQUARE)
-    left_wall = StaticSegment((0, 0), (0, HEIGHT), THICKNESS_OF_STATIC_SEGMENTS)
-    floor = StaticSegment((0, 0), (WIDTH, 0), THICKNESS_OF_STATIC_SEGMENTS)
+    # little_square = Square(SIDE_LENGTH_OF_SMALL_SQUARE, 100 + THICKNESS_OF_STATIC_SEGMENTS, SIDE_LENGTH_OF_SMALL_SQUARE/2 + THICKNESS_OF_STATIC_SEGMENTS, 1, VELOCITY_OF_SMALL_SQUARE, MASS_OF_SMALL_SQUARE)
+    # big_square = Square(SIDE_LENGTH_OF_BIG_SQUARE, SIDE_LENGTH_OF_BIG_SQUARE/2 + WIDTH - SIDE_LENGTH_OF_BIG_SQUARE, SIDE_LENGTH_OF_BIG_SQUARE/2 + THICKNESS_OF_STATIC_SEGMENTS, 2, VELOCITY_OF_BIG_SQUARE, MASS_OF_BIG_SQUARE)
+
+    little_square = Square(SIDE_LENGTH_OF_SMALL_SQUARE, 100, SIDE_LENGTH_OF_SMALL_SQUARE/2, 1, VELOCITY_OF_SMALL_SQUARE, MASS_OF_SMALL_SQUARE)
+    big_square = Square(SIDE_LENGTH_OF_BIG_SQUARE, 500, SIDE_LENGTH_OF_BIG_SQUARE/2, 2, VELOCITY_OF_BIG_SQUARE, MASS_OF_BIG_SQUARE)
+    left_wall = VerticalStaticSegment((0, 0), (0, HEIGHT), THICKNESS_OF_STATIC_SEGMENTS, 3)
+    floor = HorizontalStaticSegment((0, 0), (WIDTH, 0), THICKNESS_OF_STATIC_SEGMENTS, 4)
+    big_square.move()
+
+
+    # handler = space.add_collision_handler(1, 2)
+    handler2 = space.add_collision_handler(1, 3)
+    # handler.begin = collide
+    handler2.begin = collide
+
 
     while True:
         for event in pg.event.get():
@@ -36,16 +49,23 @@ def game():
 def convert_coordinates(point):
     return int(point[0]), HEIGHT-int(point[1])
 
+def collide(arbiter, space, data):
+    global number_of_collisions
+    number_of_collisions += 1
+    print(number_of_collisions)
+
+    return True
 
 class Square():
     def __init__(self, side_length, coordinate_x, coordinate_y, collision_type, velocity, mass):
-        self.body = pm.Body()
+        moment = pm.moment_for_box(mass, (side_length, side_length))
+        self.body = pm.Body(mass, moment)
         self.body.position = convert_coordinates((coordinate_x, coordinate_y))
         self.body.velocity = velocity
 
-        self.shape = pm.Poly.create_box(self.body, (side_length, side_length))
+        self.shape = pm.Poly.create_box(self.body, size=(side_length, side_length))
         self.shape.elasticity = 1
-        # what is density?
+        self.shape.friction = FRICTION
         self.shape.density = 1
         self.shape.collision_type = collision_type
 
@@ -55,11 +75,26 @@ class Square():
     def draw():
         space.debug_draw(draw_options)
 
+    def move(self):
+        self.body.apply_impulse_at_world_point(self.body.velocity, self.body.position)
 
-class StaticSegment():
-    def __init__(self, start, end, thickness):
+class HorizontalStaticSegment():
+    def __init__(self, start, end, thickness, collision_type):
         self.body = pm.Body(body_type=pm.Body.STATIC)
         self.shape = pm.Segment(self.body, convert_coordinates(start), convert_coordinates(end), thickness)
+        self.shape.elasticity = 0
+        self.shape.friction = 0
+        self.shape.collision_type = collision_type
+
+        space.add(self.body, self.shape)
+
+class VerticalStaticSegment():
+    def __init__(self, start, end, thickness, collision_type):
+        self.body = pm.Body(body_type=pm.Body.STATIC)
+        self.shape = pm.Segment(self.body, convert_coordinates(start), convert_coordinates(end), thickness)
+        self.shape.elasticity = 1
+        self.shape.friction = 0
+        self.collision_type = collision_type
 
         space.add(self.body, self.shape)
 
@@ -68,5 +103,8 @@ pg.init()
 display = pg.display.set_mode(RES)
 clock = pg.time.Clock()
 space = pm.Space()
+space.gravity = (0, 30000)
 draw_options = pm.pygame_util.DrawOptions(display)
+
+number_of_collisions = 0
 game()
